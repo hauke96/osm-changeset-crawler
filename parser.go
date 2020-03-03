f34dfd2ff708899099a4c82948f5565171342b16
Hauke Stieler <mail@hauke-stieler.de>
2020-03-03 02:05:14 +0100

    Add manual unmarshalling of XML
    Slightly increases performance

--- parser.go
+++ parser.go
@@ -4,6 +4,10 @@ package main
 
 import (
 	"encoding/xml"
+	"io"
+	"math"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hauke96/sigolo"
@@ -14,7 +18,7 @@ func parse(changesetStringChannel <-chan []string, changesetChannel chan<- []Cha
 	clock := time.Now()
 
 	// TODO parameter
-	amountOfCunks := 5
+	amountOfCunks := int(math.Min(10, float64(CACHE_SIZE)))
 
 	// Amount of processed changeset within the current cache. When the cache
 	// is sent to the channel, this variable will be reset
@@ -25,10 +29,14 @@ func parse(changesetStringChannel <-chan []string, changesetChannel chan<- []Cha
 
 	for changesets := range changesetStringChannel {
 		sigolo.Info("Received changesets set %d -> parsing", receivedChangesetSets)
+		sigolo.Info("  Receiving took %dms", time.Since(clock).Milliseconds())
+		clock = time.Now()
 		receivedChangesetSets++
 
+		// Parallelize parsing
 		finishChan := make(chan bool)
 		chunkSize := len(changesets) / amountOfCunks
+		sigolo.Debug("Chunk size: %d", chunkSize)
 		for chunk := 0; chunk < amountOfCunks; chunk++ {
 			startIndex := chunk * chunkSize
 			go parseChangesets(&cache, startIndex, changesets[startIndex:startIndex+chunkSize], finishChan)
@@ -39,11 +47,12 @@ func parse(changesetStringChannel <-chan []string, changesetChannel chan<- []Cha
 
 		sigolo.Info("Parsed %d changesets", CACHE_SIZE)
 		sigolo.Info("  Parsing took %dms", time.Since(clock).Milliseconds())
+		clock = time.Now()
 
 		changesetChannel <- cache
 		cache = make([]Changeset, CACHE_SIZE)
 		cacheIndex = 0
-
+		sigolo.Info("  Sending parsed data took %dms", time.Since(clock).Milliseconds())
 		clock = time.Now()
 	}
 
@@ -54,17 +63,16 @@ func parse(changesetStringChannel <-chan []string, changesetChannel chan<- []Cha
 }
 
 func parseChangesets(cache *[]Changeset, cacheIndex int, changesets []string, finishChan chan bool) {
-	name := cacheIndex
-	sigolo.Info("%d start (at %d)", name, cacheIndex)
 	for i, changesetString := range changesets {
 		// No data, no action
 		if changesetString == "" {
 			continue
 		}
 
-		var changeset Changeset
-		xml.Unmarshal([]byte(changesetString), &changeset)
+		// var changeset Changeset
+		// xml.Unmarshal([]byte(changesetString), &changeset)
 
+		changeset := unmarshal(changesetString)
 		(*cache)[cacheIndex] = changeset
 		cacheIndex++
 
@@ -72,7 +80,55 @@ func parseChangesets(cache *[]Changeset, cacheIndex int, changesets []string, fi
 		sigolo.Debug("    Receive index : %d", i)
 		sigolo.Debug("    Cache index   : %d", cacheIndex)
 	}
-	sigolo.Info("%d end (at %d)", name, cacheIndex-1)
 
 	finishChan <- true
+}
+
+func unmarshal(changeset string) Changeset {
+	decoder := xml.NewDecoder(strings.NewReader(changeset))
+
+	c := Changeset{}
+
+	for {
+		tok, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		sigolo.FatalCheck(err)
+
+		switch root := tok.(type) {
+		case xml.StartElement:
+			switch root.Name.Local {
+			case "changeset":
+				for _, a := range root.Attr {
+					switch a.Name.Local {
+					case "id":
+						i, err := strconv.Atoi(a.Value)
+						sigolo.FatalCheck(err)
+						c.Id = int64(i)
+						break
+					case editorKey:
+						c.User = a.Value
+						break
+					}
+				}
+			case "tag":
+				c.Tags = make([]Tag, len(root.Attr))
+				for i, a := range root.Attr {
+					t := Tag{}
+					switch a.Name.Local {
+					case "k":
+						t.K = a.Value
+						break
+					case "v":
+						t.V = a.Value
+						break
+					}
+					c.Tags[i] = t
+				}
+			}
+		}
+	}
+
+	return c
 }
